26487e9c5023327801d96f87e719e63ac33776ab
sergej <lupaevsv@gmail.com>
2024-08-30 21:47:03 +0700

    Initial commit

---
+++ src/main/java/com/github/lupaev/shcoolgroups/ShcoolGroupsApplication.java
@@ -0,0 +1,13 @@
+package com.github.lupaev.shcoolgroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShcoolGroupsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShcoolGroupsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lupaev/shcoolgroups/ShcoolGroupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lupaev.shcoolgroups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShcoolGroupsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
